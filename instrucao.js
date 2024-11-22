b83849a4d36b19e0f03076cd44778f3b9c3981a5
Carlos Antonio <gerrilhas@gmail.com>
2024-11-22 17:32:33 -0300

    if e switch

---
+++ instrucao.js
@@ -0,0 +1,33 @@
+var pais = "Brasil";
+
+if(pais != "Brasil"){
+    console.log("Você é estrangeiro");
+}else{
+    console.log("Você é brasileiro")
+}
+
+var idade = 22;
+
+if (idade <16){
+    console.log("não vota")
+}else if (idade < 18 || idade >65){
+    console.log("Voto opicional")
+}else{
+    console.log("Voto obrigatório")
+}
+
+var diaSem = 4;
+switch(diaSem){
+    case 1:
+        console.log('Domingo');
+        break;
+    case 2:
+        console.log("Segunda")
+        break;
+    case 3:
+    console.log("Terça");
+        break;    
+     default:
+        console.log("Esse dia da semana não existe!");
+        break;  
+}
